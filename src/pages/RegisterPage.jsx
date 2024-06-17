62bb4779e604dd4b199c2a0f6624b64618ba0439
Pablo Diaz <pablo.diaz@lamercantil.com.ar>
2024-06-17 05:19:10 -0300

    Add email validation on register form

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,11 +1,41 @@
-import { Button } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 import { useSnackbar } from "notistack";
+import { useState } from "react";
 
 export default function RegisterPage() {
 
+    const [email, setEmail] = useState("");
+    const [error, setError] = useState({
+        error: false,
+        message: "",
+    });
     const { enqueueSnackbar } = useSnackbar();
 
-    const handleClick = () => {
+    const emailValidation = (email) => {
+        const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+        return regex.test(email);
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (!email.trim()) {
+            setError({
+                error: true,
+                message: "El email es obligatorio",
+            });
+            return;
+        }
+        if (!emailValidation(email.trim())) {
+            setError({
+                error: true,
+                message: "El email no es valido",
+            });
+            return;
+        }
+        setError({
+            error: false,
+            message: "",
+        });
         enqueueSnackbar("Registro realizado con exito", {
             variant: "success",
             anchorOrigin: {
@@ -18,11 +48,32 @@ export default function RegisterPage() {
     return (
         <>
             <h1>Register</h1>
-            <Button
-                variant="contained"
-                onClick={handleClick}
+            <Box
+                component="form"
+                onSubmit={onSubmit}
+                sx={{ display: { xs: "grid", sm: "block" }, gap: 2 }}
+                noValidate
+                autoComplete="off"
             >
-                Submit
-            </Button>
+                <TextField
+                    label="Email"
+                    variant="standard"
+                    id="email"
+                    type="email"
+                    fullWidth
+                    required
+                    error={error.error}
+                    helperText={error.message}
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+                <Button
+                    variant="outlined"
+                    type="submit"
+                    sx={{ mt: 2 }}
+                >
+                    Registrarse
+                </Button>
+            </Box>
         </>);
 }
