604b86be81c623854d2b1486e0148f11720ffe0a
Pablo Diaz <pablo.diaz@lamercantil.com.ar>
2024-06-17 01:00:05 -0300

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button, Container } from "@mui/material"
+
+function App() {
+  return (
+    <Container>
+      <h1>EduTrack with Material UI</h1>
+      <Button variant="contained">Getting Started</Button>
+    </Container>
+  )
+}
+
+export default App
