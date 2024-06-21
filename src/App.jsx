645771f37b7fe109d758794d4ce6499081767b0c
Pablo Diaz <pablo.diaz@lamercantil.com.ar>
2024-06-21 15:40:02 -0300

    Temporary page for testing

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,17 @@ import { Route, Routes } from "react-router-dom";
 import CalificacionesPage from "./pages/CalificacionesPage";
 import LoginPage from "./pages/LoginPage"
 import RegisterPage from "./pages/RegisterPage"
+import Temp from "./tmp/Temp";
 
 const navLinks = [
   { title: "Home", path: "/" },
+  { title: "Admin", path: "admin" },
   { title: "Login", path: "login" },
   { title: "Register", path: "register" },
 ];
 
 function App() {
+  // TODO: Remove demo
   return (
     <SnackbarProvider
       maxSnack={3}
@@ -21,6 +24,7 @@ function App() {
       <Container sx={{ mt: 5 }}>
         <Routes>
           <Route path="/" element={<CalificacionesPage />} />
+          <Route path="/admin" element={<Temp />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
         </Routes>
